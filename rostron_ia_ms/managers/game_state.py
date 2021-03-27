2897a2daf5b36c4650131dd709bcb41a785206ac
Etienne Schmitz <contact@etienne-schmitz.com>
2021-03-27 19:57:24 +0100

    wip - continue to add some command and internal state

--- rostron_ia_ms/managers/game_state.py
+++ rostron_ia_ms/managers/game_state.py
@@ -26,43 +26,143 @@ class Command(Enum):
 
 class State(Enum):
     HALT = 1
-    START = 2
-    STOP = 3
-
+    STOP = 2
+    KICKOFF_ALLY = 3
+    KICKOFF_OPPONENT = 4
+    NORMAL_START = 5
+    FORCE_START = 6
+    RUNNING = 7
 
 class GameStateManager(ABC, Manager):
     last_gc_receive = None
+    internal_state_ = None
 
-    def __init__(self):
-        super().__init__('manual')
+    def __init__(self, name: str):
+        super().__init__(name)
 
-    def new_command_receive(self):
-        if last_gc_receive is not None and last_gc_receive == World.gc.command:
-            return false
+    def state_change(self) -> bool:
+        '''
+            See if the state is changed since the last update.
+        '''
+        if self.last_gc_receive is not None and self.last_gc_receive == World().gc.command:
+            return False
         else:
+            self.last_gc_receive == World().gc.command
             return True
 
     def update(self):
-        if self.new_command_receive():
+        if self.state_change():
+            self.callback_stop()
             self.update_state()
         self.callback_state()
 
+    def callback_stop(self):
+        if self.internal_state_ == State.HALT:
+            self.stop_halt()
+        elif self.internal_state_ == State.STOP:
+            self.stop_stop()
+        elif self.internal_state_ == State.KICKOFF_ALLY:
+            self.stop_kickoff_ally()
+        elif self.internal_state_ == State.KICKOFF_OPPONENT:
+            self.stop_kickoff_opponent()
+        
+
     def update_state(self):
-        if World().gc.stage == Command.HALT:
-            self.get_logger()
-            internal_state_ == State.HALT
-        elif World().gc.stage == Command.STOP:
-            self.stop()
+        if World().gc.command == Command.HALT:
+            self.get_logger().info('State changed : HALT')
+            self.internal_state_ == State.HALT
+            self.start_halt()
+        elif World().gc.command == Command.STOP:
+            self.get_logger().info('State changed : STOP')
+            self.internal_state_ == State.STOP
+            self.start_stop()
+        elif World().gc.command == Command.PREPARE_KICKOFF_BLUE:
+            self.get_logger().info('State changed : PREPARE_KICKOFF_BLUE')
+            if self.is_yellow:
+                self.internal_state_ == State.KICKOFF_OPPONENT
+                self.start_kickoff_opponent()
+            else:
+                self.internal_state_ == State.KICKOFF_ALLY
+                self.start_kickoff_ally()
+        elif World().gc.command == Command.PREPARE_KICKOFF_YELLOW:
+            self.get_logger().info('State changed : PREPARE_KICKOFF_YELLOW')
+            if self.is_yellow:
+                self.internal_state_ == State.KICKOFF_ALLY
+                self.start_kickoff_ally()
+            else:
+                self.internal_state_ == State.KICKOFF_OPPONENT
+                self.start_kickoff_opponent()
+        elif World().gc.command == Command.NORMAL_START:
+            self.internal_state_ == State.NORMAL_START
 
     def callback_state(self):
-        if self.internal_state_ ==
+        if self.internal_state_ == State.HALT:
+            self.halt()
+        elif self.internal_state_ == State.STOP:
+            self.stop()
+        elif self.internal_state_ == State.KICKOFF_ALLY:
+            self.kickoff_ally()
+        elif self.internal_state_ == State.KICKOFF_OPPONENT:
+            self.kickoff_opponent()
+
+    # This methods behind needs to be implemented for a match
+
+    ################################################
+    #                    HALT                      #
+    ################################################
+    @abstractmethod
+    def start_halt(self):
+        pass
 
     @abstractmethod
     def halt(self):
         pass
 
     @abstractmethod
+    def stop_halt(self):
+        pass
+
+    ################################################
+    #                    STOP                      #
+    ################################################
+    @abstractmethod
+    def start_stop(self):
+        pass
+
+    @abstractmethod
     def stop(self):
         pass
 
-    def prepare_kickoff_blue():
+    @abstractmethod
+    def stop_stop(self):
+        pass
+
+    ################################################
+    #               KICKOFF OPPONENT               #
+    ################################################
+    @abstractmethod
+    def start_kickoff_opponent(self):
+        pass
+
+    @abstractmethod
+    def kickoff_opponent(self):
+        pass
+
+    @abstractmethod
+    def stop_kickoff_opponent(self):
+        pass
+
+    ################################################
+    #                 KICKOFF ALLY                 #
+    ################################################
+    @abstractmethod
+    def start_kickoff_ally(self):
+        pass
+
+    @abstractmethod
+    def kickoff_ally(self):
+        pass
+
+    @abstractmethod
+    def stop_kickoff_ally(self):
+        pass
