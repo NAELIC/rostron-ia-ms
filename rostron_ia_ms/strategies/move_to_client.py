43443277fb623af518e811aeeb18f5791fc35df1
Etienne Schmitz <contact@etienne-schmitz.com>
2021-08-07 16:27:26 +0200

    feat - movetoclient works now !

--- rostron_ia_ms/strategies/move_to_client.py
+++ rostron_ia_ms/strategies/move_to_client.py
@@ -7,11 +7,10 @@ from rostron_interfaces.action import MoveTo
 class MoveToActionClient(Node):
     def __init__(self):
         super().__init__('move_to_action_client')
-        self._action_client = ActionClient(self, MoveTo, 'yellow/move_to')
+        self._action_client = ActionClient(self, MoveTo, 'r_0/move_to')
 
     def move_to(self, id, position, orientation):
         goal_msg = MoveTo.Goal()
-        goal_msg._id = id
 
         pos_msg = Point()
         pos_msg.x= position[0]
@@ -25,7 +24,7 @@ class MoveToActionClient(Node):
 def main(args=None):
     rclpy.init(args=args)
     action_client = MoveToActionClient()
-    future = action_client.move_to(3,(0.0,2.0),0.0)
+    future = action_client.move_to(0,(0.0, 2.0),0.0)
     rclpy.spin_until_future_complete(action_client, future)
 
 if __name__ == '__main__':
